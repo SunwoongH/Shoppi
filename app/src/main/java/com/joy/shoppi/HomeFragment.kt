01da7f893d954372106f809e18bb9363b03c9e86
SunWoong <sunwoong1122@gmail.com>
2022-08-02 01:26:04 +0900

    feat: 메인 화면 bottom navigation & 상품 상세 화면 이동 구현

---
+++ app/src/main/java/com/joy/shoppi/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.joy.shoppi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<Button>(R.id.home_example_btn)
+        btn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_productDetailFragment)
+        }
+    }
+}
--- app/src/main/java/com/joy/shoppi/MainActivity.kt
+++ app/src/main/java/com/joy/shoppi/MainActivity.kt
@@ -2,10 +2,21 @@ package com.joy.shoppi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bnv)
+        bottomNavigationView.itemIconTintList = null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.main_fcv)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
