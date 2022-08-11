85218746145caf22c6100f62863b659820a7165a
SunWoong <sunwoong1122@gmail.com>
2022-08-12 02:20:46 +0900

    feat: home repository 구현

---
+++ app/src/main/java/com/joy/shoppi/repository/HomeRepository.kt
@@ -0,0 +1,10 @@
+package com.joy.shoppi.repository
+
+import com.joy.shoppi.model.HomeData
+
+class HomeRepository(private val assetDataSource: HomeAssetDataSource) {
+
+    fun getHomeData(): HomeData? {
+        return assetDataSource.getHomeData()
+    }
+}
