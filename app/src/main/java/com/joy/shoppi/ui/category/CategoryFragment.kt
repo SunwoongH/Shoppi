66ebb4fdf9cf33d0e77ed67758d89ada53d7a889
SunWoong <sunwoong1122@gmail.com>
2022-08-13 15:33:11 +0900

    feat: category recycler view adapter 구현 & binding 적용

---
+++ app/src/main/java/com/joy/shoppi/ui/category/CategoryFragment.kt
@@ -0,0 +1,37 @@
+package com.joy.shoppi.ui.category
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.joy.shoppi.databinding.FragmentCategoryBinding
+import com.joy.shoppi.ui.common.ViewModelFactory
+
+class CategoryFragment : Fragment() {
+
+    private val viewModel: CategoryViewModel by viewModels { ViewModelFactory(requireContext()) }
+    private lateinit var binding: FragmentCategoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoryAdapter = CategoryAdapter()
+        binding.categoryRv.adapter = categoryAdapter
+        viewModel.items.observe(
+            viewLifecycleOwner
+        ) {
+            categoryAdapter.submitList(it)
+        }
+    }
+}
