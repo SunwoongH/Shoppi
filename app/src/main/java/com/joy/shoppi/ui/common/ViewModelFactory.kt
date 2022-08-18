1747fa1da623e5f70915240516271a26e1be661c
SunWoong <sunwoong1122@gmail.com>
2022-08-18 21:39:55 +0900

    feat: category detail view model 구현

--- app/src/main/java/com/joy/shoppi/ui/common/ViewModelFactory.kt
+++ app/src/main/java/com/joy/shoppi/ui/common/ViewModelFactory.kt
@@ -7,9 +7,12 @@ import com.joy.shoppi.AssetLoader
 import com.joy.shoppi.network.ApiClient
 import com.joy.shoppi.repository.category.CategoryRemoteDataSource
 import com.joy.shoppi.repository.category.CategoryRepository
+import com.joy.shoppi.repository.categorydetail.CategoryDetailRemoteDataSource
+import com.joy.shoppi.repository.categorydetail.CategoryDetailRepository
 import com.joy.shoppi.repository.home.HomeAssetDataSource
 import com.joy.shoppi.repository.home.HomeRepository
 import com.joy.shoppi.ui.category.CategoryViewModel
+import com.joy.shoppi.ui.categorydetail.CategoryDetailViewModel
 import com.joy.shoppi.ui.home.HomeViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
@@ -21,6 +24,15 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
             }
             modelClass.isAssignableFrom(CategoryViewModel::class.java) -> {
                 CategoryViewModel(CategoryRepository(CategoryRemoteDataSource(ApiClient.create()))) as T
+            }
+            modelClass.isAssignableFrom(CategoryDetailViewModel::class.java) -> {
+                CategoryDetailViewModel(
+                    CategoryDetailRepository(
+                        CategoryDetailRemoteDataSource(
+                            ApiClient.create()
+                        )
+                    )
+                ) as T
             }
             else -> {
                 throw IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
