6d9befdfdc34265258dd31e5fd736c37bd175fc3
SunWoong <sunwoong1122@gmail.com>
2022-09-04 21:50:54 +0900

    feat: product add cart 기능 구현

--- app/src/main/java/com/joy/shoppi/ui/productdetail/ProductDetailFragment.kt
+++ app/src/main/java/com/joy/shoppi/ui/productdetail/ProductDetailFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.joy.shoppi.common.KEY_PRODUCT_ID
 import com.joy.shoppi.databinding.FragmentProductDetailBinding
+import com.joy.shoppi.ui.common.EventObserver
 import com.joy.shoppi.ui.common.ViewModelFactory
 
 class ProductDetailFragment : Fragment() {
@@ -29,8 +31,10 @@ class ProductDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
 
         setView()
+        setAddCart()
         setNavigation()
     }
 
@@ -48,6 +52,19 @@ class ProductDetailFragment : Fragment() {
             binding.product = product
             adapter.submitList(product.descriptions)
         }
+    }
+
+    private fun setAddCart() {
+        viewModel.addCartEvent.observe(
+            viewLifecycleOwner, EventObserver {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("장바구니에 상품이 담겼습니다.")
+                    .setPositiveButton(
+                        "확인"
+                    ) { _, _ -> }
+                    .show()
+            }
+        )
     }
 
     private fun setNavigation() {
--- app/src/main/java/com/joy/shoppi/ui/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/joy/shoppi/ui/productdetail/ProductDetailViewModel.kt
@@ -5,18 +5,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.joy.shoppi.model.Product
+import com.joy.shoppi.repository.cart.CartRepository
 import com.joy.shoppi.repository.productdetail.ProductDetailRepository
+import com.joy.shoppi.ui.common.Event
 import kotlinx.coroutines.launch
 
-class ProductDetailViewModel(private val productDetailRepository: ProductDetailRepository) :
+class ProductDetailViewModel(
+    private val productDetailRepository: ProductDetailRepository,
+    private val cartRepository: CartRepository
+) :
     ViewModel() {
 
     private val _product = MutableLiveData<Product>()
     val product: LiveData<Product> = _product
 
+    private val _addCartEvent = MutableLiveData<Event<Unit>>()
+    val addCartEvent: LiveData<Event<Unit>> = _addCartEvent
+
     fun getProductDetail(productId: String) {
         viewModelScope.launch {
             _product.value = productDetailRepository.getProductDetail(productId)
+        }
+    }
+
+    fun addCart(product: Product) {
+        viewModelScope.launch {
+            cartRepository.addCartSectionItem(product)
+            _addCartEvent.value = Event(Unit)
         }
     }
 }
