c84cfe3fa42b8680093199fb10bf5d64af67da86
SunWoong <sunwoong1122@gmail.com>
2022-09-04 21:49:23 +0900

    feat: database provide 기능 구현

--- app/src/main/java/com/joy/shoppi/ServiceLocator.kt
+++ app/src/main/java/com/joy/shoppi/ServiceLocator.kt
@@ -1,15 +1,31 @@
 package com.joy.shoppi
 
+import android.content.Context
+import androidx.room.Room
+import com.joy.shoppi.database.AppDatabase
 import com.joy.shoppi.network.ApiClient
 
 object ServiceLocator {
 
     private var apiClient: ApiClient? = null
+    private var database: AppDatabase? = null
 
     fun provideApiClient(): ApiClient {
         return apiClient ?: kotlin.run {
             ApiClient.create().also {
                 apiClient = it
+            }
+        }
+    }
+
+    fun provideDatabase(applicationContext: Context): AppDatabase {
+        return database ?: kotlin.run {
+            Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java,
+                "shoppi-database"
+            ).build().also {
+                database = it
             }
         }
     }
